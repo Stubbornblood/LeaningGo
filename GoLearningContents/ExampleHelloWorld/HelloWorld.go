cc78a6689cb75067f024f95eec14cd2ebb944c7b
Stubbornblood <ss3591419@gmail.com>
2024-07-02 20:56:25 +0530

    Learne Variable

---
+++ GoLearningContents/ExampleHelloWorld/HelloWorld.go
@@ -0,0 +1,10 @@
+// package main
+// import ("fmt")
+
+// func main(){
+// 	fmt.Print("Hello World Go is Running!")
+// }
+
+//Go Compact Code:
+
+package main; import ("fmt"); func main(){fmt.Printf("Hello World")};
---
+++ GoLearningContents/GoVariables/DeclaringVariable.go
@@ -0,0 +1,10 @@
+package main 
+import ("fmt")
+
+func main(){
+	var student1 string = "Krishna"
+	var student2 string = "Shivanshu"
+	x := 2
+
+	fmt.Print(student1,student2,x)
+}
