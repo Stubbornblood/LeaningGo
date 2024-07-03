c890a6acbceb972a9c57c98aef894a595f6335fc
Stubbornblood <ss3591419@gmail.com>
2024-07-04 01:02:56 +0530

    Done

--- GoLearningContents/GoFunction/GoFunction.go
+++ GoLearningContents/GoFunction/GoFunction.go
@@ -23,9 +23,25 @@ func MySecondFunction(a int,b int)(result int){
 	return
 }
 
+func MyThirdFunction(a int,b int)(result int){
+	result = a+b
+	return
+}
+
+func MyFourthFunction(a int,b int)(result int , txt1 string){
+	result = a+b
+	txt1 = "Hello"
+	return
+}
 
 func main(){
 	familyNames("Liam",23)	
 	fmt.Println(Myfunction(10,20))
-	fmt.Print(MySecondFunction(20,30))
+	fmt.Println(MySecondFunction(20,30))
+	val := MyThirdFunction(40,50)
+	fmt.Println(val)
+	a,b := MyFourthFunction(100,2000)
+	fmt.Println(a," ",b)
+	_,b = MyFourthFunction(10,23)
+	fmt.Println(b)
 }
